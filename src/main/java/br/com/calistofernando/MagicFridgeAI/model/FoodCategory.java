85d738ea003131990ee67656ccb39747f150ffa1
Fernando Calisto <fernandocalisto27@gmail.com>
2026-03-24 13:59:02 -0300

    feat: create FoodItemModel entity with JPA mapping

---
+++ src/main/java/br/com/calistofernando/MagicFridgeAI/model/FoodCategory.java
@@ -0,0 +1,16 @@
+package br.com.calistofernando.MagicFridgeAI.model;
+
+public enum FoodCategory {
+    PRODUCE,
+    MEAT_AND_POULTRY,
+    SEAFOOD,
+    DAIRY,
+    GRAINS_AND_PASTA,
+    PANTRY_ESSENTIALS,
+    OILS_AND_FATS,
+    SPICES_AND_HERBS,
+    CONDIMENTS_AND_SAUCES,
+    NUTS_AND_SEEDS,
+    LIQUIDS_AND_BEVERAGES,
+    OTHER
+}
--- src/main/java/br/com/calistofernando/MagicFridgeAI/model/FoodItemModel.java
+++ src/main/java/br/com/calistofernando/MagicFridgeAI/model/FoodItemModel.java
@@ -1,4 +1,25 @@
 package br.com.calistofernando.MagicFridgeAI.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_food_item")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class FoodItemModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Enumerated(EnumType.STRING)
+    private FoodCategory foodCategory;
+    private Integer quantity;
+    private LocalDate expirationDate;
 }
