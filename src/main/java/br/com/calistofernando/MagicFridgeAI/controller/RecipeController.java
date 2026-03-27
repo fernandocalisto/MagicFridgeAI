3820d537ef32c7c0e5942007e2837fcfbe1f796d
Fernando Calisto <fernandocalisto27@gmail.com>
2026-03-27 12:36:01 -0300

    feat: implement RecipeController for generate route

--- src/main/java/br/com/calistofernando/MagicFridgeAI/controller/RecipeController.java
+++ src/main/java/br/com/calistofernando/MagicFridgeAI/controller/RecipeController.java
@@ -1,5 +1,8 @@
 package br.com.calistofernando.MagicFridgeAI.controller;
 
+import br.com.calistofernando.MagicFridgeAI.dto.RecipeResponseDTO;
+import br.com.calistofernando.MagicFridgeAI.model.FoodItemModel;
+import br.com.calistofernando.MagicFridgeAI.service.FoodItemService;
 import br.com.calistofernando.MagicFridgeAI.service.GeminiService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -7,16 +10,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 
 public class RecipeController {
 
     private final GeminiService geminiService;
+    private final FoodItemService foodItemService;
 
-    @GetMapping
-    public Mono<ResponseEntity<String>> generateRecipe() {
-        return geminiService.generateRecipe();
+    @GetMapping("/generate")
+    public Mono<ResponseEntity<RecipeResponseDTO>> generateRecipe() {
+        List<FoodItemModel> foodItemModelList = foodItemService.getAllFoodItem();
+        return geminiService.generateRecipe(foodItemModelList)
+                .map(recipe -> ResponseEntity.ok(recipe))
+                .defaultIfEmpty(ResponseEntity.noContent().build());
     }
 
 }
